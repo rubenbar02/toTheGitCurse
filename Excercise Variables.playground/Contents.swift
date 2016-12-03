b5901512726522c5e70236fa8522c1b93da11fc9
rubenbar02@gmail.com <rbarboza@Rubens-MacBook-Pro.local>
2016-12-03 09:54:22 -0300

    FirsCommit

---
+++ Excercise Variables.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number1 = 15
+var number2 = 5
+var resultSum = number1 + number2
+var multiplicationRsult = 9 * number2
+var restRestult = number1 - number2
+var dvideResut = number1 / number2
+var a = 1, b = 2, c = 3
+let maxA = 5, maxB = 5, maxC = 10
+var doubleNumber: Double
+
+var number4: Int = 423
+var helloString = "Hello"
+var wordString = "World"
+var addedString = helloString + wordString
+var interpolationStrig = "\(helloString) \(wordString)"
+
+
+
+
